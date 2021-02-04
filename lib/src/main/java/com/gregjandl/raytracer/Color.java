54b8f6aa799b7d5c907f17103776efd24705ceb7
sindisil <greg.jandl@gmail.com>
2021-02-03 18:21:19 -0600

    Implement Color class
    Was going to use java.awt.Color, but to keep
    consistent with book (which lets color component
    values act as a sort of HDR by letting the value
    range outside 0-1), I'm going with this implementation.
    
    I did add a method to convert to an AWT Color, though.
    
    May refactor to eliminate this class later, if the
    "HDR" aspect doesn't seem useful.

---
+++ lib/src/main/java/com/gregjandl/raytracer/Color.java
@@ -0,0 +1,77 @@
+package com.gregjandl.raytracer;
+
+import java.util.Objects;
+
+public class Color {
+  private final float r;
+  private final float g;
+  private final float b;
+
+  public static final Color BLACK = new Color(0, 0, 0);
+  public static final Color WHITE = new Color(1, 1, 1);
+  public static final Color RED = new Color(1, 0, 0);
+  public static final Color GREEN = new Color(0, 1, 0);
+  public static final Color BLUE = new Color(0, 0, 1);
+  public static final Color YELLOW = new Color(1, 1, 0);
+  public static final Color MAGENTA = new Color(1, 0, 1);
+  public static final Color CYAN = new Color(0, 1, 1);
+  public static final Color LT_GRAY = new Color(.25f, .25f, .25f);
+  public static final Color GRAY = new Color(.5f, .5f, .5f);
+  public static final Color DK_GRAY = new Color(.75f, .75f, .75f);
+
+  public Color(float r, float g, float b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+  }
+
+  public float getR() { return r;}
+
+  public float getG() { return g;}
+
+  public float getB() { return b;}
+
+  private float clamp(float val) {
+    return val < 0 ? 0 : val > 1 ? 1 : val;
+  }
+
+  public java.awt.Color toAwtColor() {
+    return new java.awt.Color(clamp(r), clamp(g), clamp(b));
+  }
+
+  @Override
+  public String toString() {
+    return "Color{r=" + r + ", g=" + g + ", b=" + b + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) { return true; }
+    if (o == null || getClass() != o.getClass()) { return false; }
+    Color color = (Color) o;
+    return Utils.aboutEqual(color.r, r) && Utils.aboutEqual(color.g, g)
+        && Utils.aboutEqual(color.b, b);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(r, g, b);
+  }
+
+  public Color add(Color c2) {
+    return new Color(r + c2.r, g + c2.g, b + c2.b);
+  }
+
+  public Color subtract(Color c2) {
+    return new Color(r - c2.r, g - c2.g, b - c2.b);
+  }
+
+  public Color multiply(float n) {
+    return new Color(r * n, g * n, b * n);
+  }
+
+  public Color multiply(Color c2) {
+    return new Color(r * c2.r, g * c2.g, b * c2.b);
+  }
+}
+
---
+++ lib/src/test/java/com/gregjandl/raytracer/ColorTest.java
@@ -0,0 +1,60 @@
+package com.gregjandl.raytracer;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ColorTest {
+  @Test
+  @DisplayName("Color creation")
+  void testColorCreation() {
+    var c = new Color(-0.5f, 0.4f, 1.7f);
+    assertEquals(Float.compare(-.5f, c.getR()), 0);
+    assertEquals(Float.compare(.4f, c.getG()), 0);
+    assertEquals(Float.compare(1.7f, c.getB()), 0);
+  }
+
+  @Test
+  @DisplayName("A Color may be converted to an AWT Color")
+  void testConvertToAwtColor() {
+    var c = Color.GRAY;
+    assertEquals(java.awt.Color.GRAY, c.toAwtColor());
+  }
+
+  @Test
+  @DisplayName("Colors may be added")
+  void testAdd() {
+    var c1 = new Color(.9f, .6f, .75f);
+    var c2 = new Color(.7f, .1f, .25f);
+    assertEquals(new Color(1.6f, .7f, 1), c1.add(c2));
+    assertEquals(new Color(1.6f, .7f, 1), c2.add(c1));
+  }
+
+  @Test
+  @DisplayName("Colors may be subtracted")
+  void testSubtract() {
+    var c1 = new Color(.9f, .6f, .75f);
+    var c2 = new Color(.7f, .1f, .25f);
+    assertEquals(new Color(.2f, .5f, .5f), c1.subtract(c2));
+    assertEquals(new Color(-.2f, -.5f, -.5f), c2.subtract(c1));
+  }
+
+  @Test
+  @DisplayName("A Color my be multiplied by a scalar")
+  void testMultiplyByScalar() {
+    var c = new Color(.2f, .3f, .4f);
+    assertEquals(new Color(.4f, .6f, .8f), c.multiply(2));
+  }
+
+  @Test
+  @DisplayName("Colors may be multiplied")
+  void testMultiply() {
+    var c1 = new Color(1, .2f, .4f);
+    var c2 = new Color(.9f, 1, .1f);
+    assertEquals(new Color(.9f, .2f, .04f), c1.multiply(c2));
+    assertEquals(new Color(.9f, .2f, .04f), c2.multiply(c1));
+  }
+
+}
