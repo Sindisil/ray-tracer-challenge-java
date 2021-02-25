52bc263200b37a4ec5a328ad8dc2915a76152c4b
sindisil <greg.jandl@gmail.com>
2021-02-24 18:00:45 -0600

    Add toString(), equals(), and hashCode() to PointLight

--- lib/src/main/java/com/gregjandl/raytracer/rtlib/PointLight.java
+++ lib/src/main/java/com/gregjandl/raytracer/rtlib/PointLight.java
@@ -1,5 +1,7 @@
 package com.gregjandl.raytracer.rtlib;
 
+import java.util.Objects;
+
 /**
  * Represents a point light source.
  */
@@ -42,5 +44,42 @@ public class PointLight {
    */
   public Color getColor() {
     return color;
+  }
+
+  /**
+   * Returns a string representation of this {@code PointLight}. This method is intended to be used for
+   * debugging purposes; the representation may change, but will not be {@code null}.
+   *
+   * @return a string representation of this {@code PointLight}
+   */
+  @Override
+  public String toString() {
+    return "PointLight{position=" + position + ", color=" + color + '}';
+  }
+
+  /**
+   * Compares this {@code PointLight} with the specified {@code Object} for equality.
+   *
+   * @param o {@code Object} to which this {@code PointLight} is to be compared.
+   * @return {@code true} if and only if the specified {@code Object} is a {@code PointLight} whose
+   * position and color are equal to this {@code PointLight}'s
+   */
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) { return true; }
+    if (o == null || getClass() != o.getClass()) { return false; }
+    PointLight that = (PointLight) o;
+    return Objects.equals(position, that.position)
+        && Objects.equals(color, that.color);
+  }
+
+  /**
+   * Returns the hash code for this {@code PointLight}
+   *
+   * @return hash code for this {@code PointLight}
+   */
+  @Override
+  public int hashCode() {
+    return Objects.hash(position, color);
   }
 }
